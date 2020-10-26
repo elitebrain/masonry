ca6bd33485f32c216c58566aa3afadc7ead9f5ee
elitebrain <elitebrain@naver.com>
2020-10-26 17:04:47 +0900

    loading anim, set useCallback at func

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 function App() {
   const [list, setList] = useState([]);
-  useEffect(() => {
-    _setPosition();
-    window.addEventListener("resize", _setPosition);
-    let _list = [];
-    const imgId = [1011, 883, 64, 1074, 823, 92, 643, 200, 412, 456, 210, 334];
-    imgId.push(0);
-    imgId.push(65);
-    imgId.push(0);
-    imgId.push(839);
-    imgId.push(0);
-    for (let i = 0; i < imgId.length; i++) {
-      const ih = 200 + Math.floor(Math.random() * 10) * 15;
-      if (imgId[i] === 0) {
-        _list.push({ id: i, src: null, title: `제목 ${i}`, contents: `내용\ncontents\nabcdefg\n${Math.random() * 100000000000000000}` });
-      } else {
-        _list.push({ id: i, src: "https://unsplash.it/250/" + ih + "?image=" + imgId[i], title: imgId[i], contents: "" });
-      }
-    };
-    setList(_list);
-  }, [])
-  const _setPosition =  async () => {
+  const [positionSet, setPositionSet] = useState(false);
+  const _getColumns = useCallback((w) => {
+    const iw = window.innerWidth;
+    let column;
+    if (iw < 576) {
+      column = 1;
+    } else if (iw < 768) {
+      column = 2;
+    } else if (iw < 992) {
+      column = 3;
+    } else if (iw < 1200) {
+      column = 4;
+    } else {
+      column = 6;
+    }
+    return column;
+  }, []);
+  const _setPosition = useCallback(async () => {
     const columnCount = _getColumns();
     const columnList = document.querySelectorAll(".masonry > .column");
     const sumColHeight = [];
@@ -44,7 +41,30 @@ function App() {
     }
     const maxHeight = Math.max.apply(null, sumColHeight);
     document.querySelector(".masonry").style.height = `${maxHeight}px`;
-  }
+    if (maxHeight > 0) {
+      setPositionSet(true);
+    }
+  }, [_getColumns]);
+  useEffect(() => {
+    _setPosition();
+    window.addEventListener("resize", _setPosition);
+    let _list = [];
+    const imgId = [1011, 883, 64, 1074, 823, 92, 643, 200, 412, 456, 210, 334];
+    imgId.push(0);
+    imgId.push(65);
+    imgId.push(0);
+    imgId.push(839);
+    imgId.push(0);
+    for (let i = 0; i < imgId.length; i++) {
+      const ih = 200 + Math.floor(Math.random() * 10) * 15;
+      if (imgId[i] === 0) {
+        _list.push({ id: i, src: null, title: `제목 ${i}`, contents: `내용\ncontents\nabcdefg\n${Math.random() * 100000000000000000}` });
+      } else {
+        _list.push({ id: i, src: "https://unsplash.it/250/" + ih + "?image=" + imgId[i], title: `제목 ${i}`, contents: "" });
+      }
+    };
+    setList(_list);
+  }, [_setPosition]);
   const _moreList = () => {
     const _id = list.length;
     const _list = [];
@@ -55,49 +75,45 @@ function App() {
     };
     setList(prevState => prevState.concat(_list));
   }
-  const _getColumns = (w) => {
-    const iw = window.innerWidth;
-    let column;
-    if (iw < 576) {
-      column = 1;
-    } else if (iw < 768) {
-      column = 2;
-    } else if (iw < 992) {
-      column = 3;
-    } else if (iw < 1200) {
-      column = 4;
-    } else {
-      column = 6;
-    }
-    return column;
-  };
-  const Title = ({ item }) => {
+  const Card = ({ item }) => {
     const { src, title, contents, id } = item;
     return (
-      <div className="title">
-        {src ? 
+      <div className="card">
+        {src ?
           <figure onClick={() => window.open(src)}>
             <img id={id} src={src} alt={src.split("?")[1]} onLoad={_setPosition} />
-            <figcaption>{id}</figcaption>
+            <figcaption>{title}</figcaption>
           </figure>
-        :
-          <div className="card">
-            <h4>{title}</h4>
-            <p>{contents}</p>
+          :
+          <div>
+            <h4 className="title">{title}</h4>
+            <p className="contents">{contents}</p>
           </div>
         }
       </div>
     );
   };
+  const _handleCard = () => {
+
+  }
   return (
     <div className="masonry-container">
       <div className="masonry">
+        {!positionSet && <div className="loading" />}
         {list.map((item) => (
-          <div className="column col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2" key={item.id}>
-            <Title item={item} />
+          <div className="column col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2" style={{ opacity: positionSet ? 1 : 0 }} key={item.id}>
+            <Card item={item} />
           </div>
         ))}
         <button className="btn_circle" onClick={() => _moreList()}>more</button>
+      </div>
+      <div className="modal">
+        <div className="modal__overlay" />
+        <div className="modal__content">
+          <div className="modal_title"></div>
+          <div className="modal_img"></div>
+          <div className="modal_contents"></div>
+        </div>
       </div>
     </div>
   );
